43304517e0942db001153d9814042951f6467502
moustafarefaat <$cala5B@sha!>
2019-06-23 12:35:47 -0400

    Code cleanup

--- sfsynchwithretrypattern/src/main/java/sfsynchwithretrypattern/StaticHelpers.java
+++ sfsynchwithretrypattern/src/main/java/sfsynchwithretrypattern/StaticHelpers.java
@@ -9,9 +9,5 @@ public  class StaticHelpers {
 	    Duration duration = Duration.between(now, lastTime.toLocalDateTime());
 	    return Math.abs(duration.toMinutes());
 	}
-	public static long getDurationLocal(LocalDateTime lastTime) {
-		//LocalDateTime now = LocalDateTime.now();
-	    //Duration duration = Duration.between(now, lastTime);
-	    return 60; // Math.abs(duration.toMinutes());
-	}
+	
 }
